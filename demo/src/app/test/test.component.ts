42ee5120877d1a8b9c35b19af2170f13c2861be8
Arch-enemy <csiddu98@gmail.com>
2020-12-28 15:36:58 +0530

    beginning of Angular

---
+++ demo/src/app/test/test.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  age : number;
+  name : string ;
+  salary : number ;
+  hobbies : any; // array of any 
+  address : any ;
+
+  constructor() { 
+    this.age = 20 ;
+    this.name = 'sid' ;
+    this.salary = 2000000 ;
+    this.hobbies = [' basketball ' , ' art ' , ' movie critique '];
+    this.address = {doorNo:' 10-10 ' , street : ' b-36 ' , colony : ' d.d.colony '};
+    
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
